235a3c9e606f2a0c7b76fa5a6c2c67917abdcc86
sumitpahadi <sumitrawat1102000@gmail.com>
2024-01-27 17:32:55 +0530

    2

--- backend/model/User.js
+++ backend/model/User.js
@@ -1,18 +1,3 @@
-// const mongoose = require("mongoose");
-// const userschema =  new mongoose.Schema({
-//   name: {
-//     required: true,
-//     type: String,
-//   },
-//   email: {
-//     required: true,
-//     type: String,
-//   },
-// });
-
-// const user = mongoose.model("user", userschema);
-// module.exports = user;
-
 const mongoose = require("mongoose");
 const userschema =  new mongoose.Schema({
   name: {
@@ -23,12 +8,27 @@ const userschema =  new mongoose.Schema({
     required: true,
     type: String,
   },
-  rightsid:{
-    type:mongoose.Schema.Types.ObjectId,
-   
-    ref:"rights"
-  }
 });
 
 const user = mongoose.model("user", userschema);
 module.exports = user;
+
+// const mongoose = require("mongoose");
+// const userschema =  new mongoose.Schema({
+//   name: {
+//     required: true,
+//     type: String,
+//   },
+//   email: {
+//     required: true,
+//     type: String,
+//   },
+//   rightsid:{
+//     type:mongoose.Schema.Types.ObjectId,
+   
+//     ref:"rights"
+//   }
+// });
+
+// const user = mongoose.model("user", userschema);
+// module.exports = user;
